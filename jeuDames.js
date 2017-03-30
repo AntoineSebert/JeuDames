342795bc4713db592f2612e7141e3e5cedb724c8
nfossurier <nathan.fossurier@hotmail.fr>
2017-03-30 09:26:42 +0200

    Ajout cadrillage et bon nombre de pion

--- jeuDames.js
+++ jeuDames.js
@@ -1,31 +1,51 @@
 window.onload = function remplir() {
     var tab = "";
     var couleur;
-    for (var i = 1; i < 9; i++) {   //lignes
+    for (var i = 0; i < 10; i++) {   //lignes
         tab += "<tr>";
-        for (var j = 1; j < 9; j++) {   //colonnes
+        for (var j = 0; j < 10; j++) {   //colonnes
             tab += "<td id=cell-c" + j + "-l" + i + " class='";
-            ((i + j) % 2) ? couleur = "noire" : couleur = "blanche";
-            tab += couleur + "'>";
-            switch (i) {
-                //BLACK
-                case 1:
-                    tab += "<img id=img-c" + j + "-l1 src='pic/usa.jpg'>";
-                    break;
 
-                case 2:
-                    tab += "<img id=img-c" + j + "-l2 src='pic/usa.jpg'>";
-                    break;
+            if((i+j) % 2 ==1){
+                couleur = "noire";
+                tab += couleur + "'>";
+                switch (i) {
+                    //BLACK
+                    case 0:
+                        tab += "<img id=img-c" + j + "-l0 src='pic/usa.jpg'>";
+                        break;
+                    case 1:
+                        tab += "<img id=img-c" + j + "-l1 src='pic/usa.jpg'>";
+                        break;
+                    case 2:
+                        tab += "<img id=img-c" + j + "-l2 src='pic/usa.jpg'>";
+                        break;
+                    case 3:
+                        tab += "<img id=img-c" + j + "-l2 src='pic/usa.jpg'>";
+                        break;
 
-                //WALTER WHITE
-                case 7:
-                    tab += "<img id=img-c" + j + "-l7 src='pic/urss.jpg'>";
-                    break;
-                case 8:
-                    tab += "<img id=img-c" + j + "-l8 src='pic/urss.jpg'>";
-                    break;
+                    //WALTER WHITE
+                    case 6:
+                        tab += "<img id=img-c" + j + "-l8 src='pic/urss.jpg'>";
+                        break;
+                    case 7:
+                        tab += "<img id=img-c" + j + "-l8 src='pic/urss.jpg'>";
+                        break;
+                    case 8:
+                        tab += "<img id=img-c" + j + "-l9 src='pic/urss.jpg'>";
+                        break;
+                    case 9:
+                        tab += "<img id=img-c" + j + "-l10 src='pic/urss.jpg'>";
+                        break;
 
+                }
+
+            }else{
+                couleur = "blanche";
+                tab += couleur + "'>";
             }
+
+
             // document.getElementById("img-c;" + j + "-l"+i).addEventListener("mouseover", myScript);
         }
         tab += "</td>";
